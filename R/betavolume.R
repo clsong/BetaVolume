1c585c5591518b8a1df770cd3ca73d6b58a42506
clsong <chuliangsong1994@gmail.com>
2021-09-27 11:34:31 -0400

    update the beta_volume function

--- R/betavolume.R
+++ R/betavolume.R
@@ -1,34 +1,42 @@
-#' Load a Matrix
+#' Load a Matrix corresponds to metacommunity composition
 #'
-#' This function loads a file as a matrix. It assumes that the first column
-#' contains the rownames and the subsequent columns are the sample identifiers.
-#' Any rows with duplicated row names will be dropped with the first one being
-#' kepted.
-#'
-#' @param infile Path to the input file
-#' @return A matrix of the infile
+#' This function loads a file as a matrix.
+#' It assumes that columns correspond to local communities (plots or patches)
+#' while the columns correspond to species.
+#' The matrix entries correspond to species property (e.g, presence/absence).
+#' @importFrom magrittr `%>%`
+#' @import dplyr
+#' @import tidyr
+#' @import geometry
+#' @param meta_composition metacommunity composition
+#' @param weights whether the hyper points shouldbe weightedß
+#' @return the geometric measure of beta_diversity
 #' @export
-weight_composition <- function(meta_composition) {
-  weight <- meta_composition %>%
-    as_tibble() %>%
-    group_by(across()) %>%
-    mutate(n = n()) %>%
-    ungroup() %>%
-    mutate(n = nrow(.) * n / sum(n)) %>%
-    pull(n)
-  for (i in 1:nrow(meta_composition)) {
-    meta_composition[i, ] <- weight[i] * meta_composition[i, ]
+beta_volume <- function(meta_composition, weights = T) {
+  weight_composition <- function(meta_composition) {
+    weight <- meta_composition %>%
+      {suppressMessages(as_tibble(., .name_repair = "unique"))} %>%
+      group_by(across()) %>%
+      mutate(n = n()) %>%
+      ungroup() %>%
+      mutate(n = nrow(.) * n / sum(n)) %>%
+      pull(n)
+    for (i in 1:nrow(meta_composition)) {
+      meta_composition[i, ] <- weight[i] * meta_composition[i, ]
+    }
+    meta_composition
   }
-  meta_composition
-}
 
-beta_volume <- function(meta_composition){
+  P <- ncol(meta_composition)
+  if (weights) meta_composition <- weight_composition(meta_composition)
   tryCatch(
     {
-      convhulln(comp.dat_binary[[i]], output.options=TRUE)$vol
+      P * (convhulln(meta_composition, output.options = TRUE)$vol)^(1/P)
     },
     error = function(e) {
       0
     }
   )
 }
+
+
