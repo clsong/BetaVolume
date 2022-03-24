bd72989718d968e20d6fd740354c5b6c12e8c01e
Chuliang Song <chuliangsong1994@gmail.com>
2022-03-24 15:36:18 -0400

    change hypernormal estimation. To account for the corner case of too many duplications

--- R/betavolume.R
+++ R/betavolume.R
@@ -4,17 +4,16 @@
 #' It assumes that columns correspond to local communities (plots or patches)
 #' while the columns correspond to species.
 #' The matrix entries correspond to species property (e.g, presence/absence).
-#' @importFrom magrittr `%>%`
-#' @import dplyr
-#' @import tidyr
-#' @import geometry
-#' @import MVNH
-#' @import purrr
 #' @param meta_composition metacommunity composition
-#' @param weights whether the hyper points shouldbe weightedß
-#' @return the geometric measure of beta_diversity
+#' @param weights whether non-unique compositions should be weighted
+#' @param method which method to use to compute the hypervolume
+#' @param dim_threshold the threshold of which default method is used
+#' @return the value of the geometric measure of beta_diversity
 #' @export
-beta_volume <- function(meta_composition, weights = T, method, dim_threshold = 10) {
+beta_volume <- function(meta_composition,
+                        weights = T,
+                        method,
+                        dim_threshold = 10) {
   if (nrow(meta_composition) < ncol(meta_composition)) {
     meta_composition <- t(meta_composition)
   }
@@ -24,25 +23,34 @@ beta_volume <- function(meta_composition, weights = T, method, dim_threshold = 1
 
   meta_composition <- rbind(meta_composition, rep(0, P))
 
-  estiamte_volume <- function(meta_composition, method, dimension){
-    if(method == 'deterministic'){
-      hypervolume <-tryCatch(
+  estiamte_volume <- function(meta_composition, method, dimension) {
+    if (method == "deterministic") {
+      hypervolume <- tryCatch(
         {
-           P *(convhulln(meta_composition, output.options = TRUE)$vol)^(1 / P)
+          P * (convhulln(meta_composition, output.options = TRUE)$vol)^(1 / P)
         },
         error = function(e) {
-           0
+          0
         }
       )
     }
-    if(method == 'hyper_normal'){
-      hypervolume <- P*(MVNH_det(meta_composition)[1])^(1/P)*4
+    if (method == "hyper_normal") {
+      meta_composition <- unique(meta_composition, MARGIN = 1)
+      meta_composition <- unique(meta_composition, MARGIN = 2)
+
+      if (nrow(meta_composition) < ncol(meta_composition)) {
+        meta_composition <- t(meta_composition)
+      }
+
+      P <- ncol(meta_composition)
+
+      hypervolume <- P * (MVNH_det(meta_composition)[1])^(1 / P) * 4
     }
     hypervolume
   }
 
-  if(missing(method)){
-     method <- ifelse(P > dim_threshold, 'hyper_normal', 'deterministic')
+  if (missing(method)) {
+    method <- ifelse(P > dim_threshold, "hyper_normal", "deterministic")
   }
 
   estiamte_volume(meta_composition, method, dimension = P)
