5b072f085647f3628a78b7890b44dd980e297234
clsong <clsong.ecology@gmail.com>
2022-04-15 13:29:42 -0400

    add missing parmater "remove_unique"

--- R/betavolume.R
+++ R/betavolume.R
@@ -9,6 +9,7 @@
 #' @param weights whether non-unique compositions should be weighted
 #' @param hypervolume_method which method to estimate the hypervolume
 #' @param dim_threshold the threshold of which default hypervolume_method is used
+#' @param remove_unique whether unique compositions should be removed
 #' @return the value of the geometric measure of beta_diversity
 #' @export
 betavolume <- function(meta_composition,
@@ -16,7 +17,6 @@ betavolume <- function(meta_composition,
                        hypervolume_method,
                        dim_threshold = 10,
                        remove_unique = F) {
-
   weight_composition <- function(meta_composition) {
     meta_composition <- meta_composition[which(rowSums(meta_composition) != 0), ]
     weight <- meta_composition %>%
