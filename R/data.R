e435832ed27c1a5efdc8919d50cd00e0112ddb25
clsong <clsong.ecology@gmail.com>
2022-04-15 16:36:45 -0400

    change default data set

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' An example empirical dataset of metacommunity collected by A. H. Gentry and numerous additional collectors
 #'
-#' @format A matrix with 81 rows and 10 variables:
+#' @format A matrix with 37 rows and 10 variables:
 #' @source \url{http://doi.org/10.1126/science.1208584}
 "example_meta_composition"
